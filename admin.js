3bd13685c85480d60788a4c979ee053ea95be308
Abhishek Dhanani <abhishek9712a@gmail.com>
2024-02-21 00:46:40 +0530

    update admin script

--- admin.js
+++ admin.js
@@ -6,16 +6,22 @@ async function init() {
   await admin.connect();
   console.log("Adming Connection Success...");
 
+  try {
+    console.log("Deleting Topic [rider-updates]");
+    await admin.deleteTopics({ topics: ["rider-updates"], timeout: 3000 });
+    console.log("Topic Deleted Success [rider-updates]");
+  } catch (error) {
+    console.log("Error Deleting Topic [rider-updates]", error);
+  }
+  const topics = await admin.listTopics();
+  console.log("Topics", topics);
+
   console.log("Creating Topic [rider-updates]");
-  // await admin.deleteTopics({ topics: ["rider-updates"], timeout: 3000 });
-  console.log(await admin.listTopics());
-  console.log("Topic Deleted Success [rider-updates]");
   await admin.createTopics({
     topics: [
       {
         topic: "rider-updates",
         numPartitions: 2,
-        
       },
     ],
   });
